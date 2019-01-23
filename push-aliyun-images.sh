a376df8feba409f8215eb9f8d94c23ec646b374b
will <524869004@qq.com>
2019-01-23 14:51:45 +0800

    update

--- push-aliyun-images.sh
+++ push-aliyun-images.sh
@@ -4,6 +4,10 @@ for imagepath in $(cat ./imagepath.txt)
 do
 imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
 docker pull $imagepath
+# push到阿里云仓库
 docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
 docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
+# push到dockerhub
+docker tag $imagepath willdockerhub/$imagename
+docker push willdockerhub/$imagename
 done
