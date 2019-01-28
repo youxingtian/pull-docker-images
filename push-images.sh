6c0026ef289df50dd34db8b0e5aa1f06d9afaf78
will <524869004@qq.com>
2019-01-28 22:18:07 +0800

    test

--- push-images.sh
+++ push-images.sh
@@ -5,8 +5,8 @@ do
 imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
 docker pull $imagepath
 # push到阿里云仓库
-#docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
-#docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
+docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
+docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
 # push到dockerhub
 docker tag $imagepath willdockerhub/$imagename
 docker push willdockerhub/$imagename
