dab7ef05059f0c69ce2eac9cbeb6884dfa43b92f
willgithub <524869004@qq.com>
2019-05-03 10:06:55 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#push google or coreos images to dockerhub or aliyun registry。
-#change the registry path and username/password to yourself's。
+#push google or coreos images to dockerhub or aliyun registry,
+#change the registry name and username/password to yourself's.
 
 set -e
 set -u
 
-for imagepath in $(cat ./imagepath.txt)
+DOCKER_HUB=willdockerhub
+ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/aliwill
+
+for image in $(cat img-list.txt)
 do
-imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
-docker pull $imagepath
-docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
-docker tag $imagepath willdockerhub/$imagename
-# push到阿里云仓库
-docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename && \
-# push到dockerhub
-docker push willdockerhub/$imagename
+	imagename=$(echo $image | awk -F '/' '{print $NF}')
+	docker pull $image
+	docker tag $image $ALI_REGISTRY/$imagename
+	docker tag $image $DOCKER_HUB/$imagename
+	# push到阿里云仓库
+	docker push $ALI_REGISTRY/$imagename && \
+	# push到dockerhub
+	docker push $DOCKER_HUB/$imagename
 done
