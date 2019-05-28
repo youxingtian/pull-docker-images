a1fdaff9cf6284f67b6861c5b90e91ea81c3f5b1
willgithub <524869004@qq.com>
2019-05-28 22:32:00 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -7,8 +7,9 @@ set -ev
 DOCKER_HUB=willdockerhub
 ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/aliwill
 
+#config the following env at travis-ui
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker login -u $ALI_USERNAME registry.cn-hangzhou.aliyuncs.com -p $ALI_PASSWORD
+docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
 
 for image in $(cat img-list.txt)
 do
